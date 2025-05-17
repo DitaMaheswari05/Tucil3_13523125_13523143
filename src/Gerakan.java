a1c5068f5da348cebfedb8168f0323eb2c0d5901
DitaMaheswari05 <ditamaheswari05@gmail.com>
2025-05-17 11:05:33 +0700

    feat : add movement

--- src/Gerakan.java
+++ src/Gerakan.java
@@ -1,3 +1,34 @@
 public class Gerakan {
+    private String pieceID;
+    private Direction direction;
 
+    public Gerakan(String pieceID, Direction direction) {
+        this.pieceID = pieceID;
+        this.direction = direction;
+    }
+    public String getPieceID() {
+        return pieceID;
+    }
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public String toString() {
+            String dirString = "";
+            switch (direction) {
+                case UP:
+                    dirString = "atas";
+                    break;
+                case DOWN:
+                    dirString = "bawah";
+                    break;
+                case LEFT:
+                    dirString = "kiri";
+                    break;
+                case RIGHT:
+                    dirString = "kanan";
+                    break;
+            }
+            return pieceID + "-" + dirString;
+    }
 }
